76ffcbf9ebc8e43752d058494a3ea3d20cf97a6d
ERICA CECILIATO <34410796801@jpmorgan.sp>
2018-01-19 20:49:14 -0200

    exemplos de js

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,4 @@
-var note = {
+/*var note = {
     title: ""
 };
 
@@ -10,4 +10,53 @@ function comDom()
     console.log(note);
     
 }
+
+*/
+
+//declarar variável/array global
+
+var notes = [];
+
+//atualizar a tela
+
+//resetar o formulario
+
+
+
+//atualizar tela (criar função pra atualizar tela)
+
+function atualizaTela(secao){
+    código atualiza tela
+}
+
+//limpar formulario
+form.reset
+
+
+//aplicar função de incluir os campos digitados + adicionar a lista vazia => notes
+
+function onInputDados() {
+
+    var dados = ""; // guardar html 
+    
+    for(var index=0; dados < index.lenght; index++) {
+        +=
+    }
+
+//chama aqui
+
+
+}
+
+notes.dados 
+
+for 
+
+
+
+//aplicar evento de salvar pull.notes
+
+//aplicar função deletar
+
+
 
